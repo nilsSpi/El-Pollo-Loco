a76e645ed3d1d64da15004bbe392ece7f55386c1
nils <nils.christian@web.de>
2021-12-05 18:25:53 +0100

    background Objects

--- models/backgroundObject.class.js
+++ models/backgroundObject.class.js
@@ -1,11 +1,11 @@
 class BackgroundObject extends MovableObject
 {
     width=720;
-    height=350;
-    constructor(imgPath,x,y)
+    height=480;
+    constructor(imgPath,x)
     {
         super().loadImage(imgPath);
         this.x=x;
-        this.y=y;
+        this.y=480-this.height;
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -6,7 +6,10 @@ character= new Character();
 canvas;
 
 backgroundObjects=[
-    new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png',0,100)
+    new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png',0),
+    new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png',0),
+    new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png',0),
+    new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png',0)
 ];
 
 clouds=[
