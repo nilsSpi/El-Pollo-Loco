6e1e9562b8b88d599256f0ad5d6c86f6692e3ca4
nils <nils.christian@web.de>
2021-12-05 18:35:26 +0100

    some scaling of character and basic enemys

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,6 @@
 class Character extends MovableObject{
-   
+   height=280;
+   y=180;
     constructor(){
         super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
 
