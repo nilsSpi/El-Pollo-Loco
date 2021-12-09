c1cc1227b22bd56800686542c64c29b9443d91e2
nils <nils.christian@web.de>
2021-12-09 23:18:18 +0100

    struc lvl1

--- models/world.class.js
+++ models/world.class.js
@@ -2,28 +2,17 @@ class World {
 ctx;
 keyboard;
 
+camera_x=0;
+
 character= new Character();
 
 canvas;
 
-backgroundObjects=[
-    new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png',0),
-    new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png',0),
-    new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png',0),
-    new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png',0)
-];
+backgroundObjects=level1.backgroundObjects;
 
-clouds=[
-    new Cloud()
-];
+clouds=level1.clouds;
    
-enemies=[
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-
-
-];
+enemies=level1.enemies;
 
     constructor(canvas,keyboard){
 this.ctx=canvas.getContext('2d');
@@ -40,12 +29,13 @@ this.setWorld();
 
     draw(){
         this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
-       
+       this.ctx.translate(this.camera_x,0);
 
 this.addArrayToMap(this.backgroundObjects);
 this.addArrayToMap(this.clouds);
 this.addArrayToMap(this.enemies);
 this.addToMap(this.character);
+this.ctx.translate(-this.camera_x,0);
 
 let self=this;
 requestAnimationFrame( function (){self.draw();})
@@ -72,6 +62,42 @@ array.forEach(object=>{
         {
             object.x=object.x*-1;
             this.ctx.restore();
+        }
+    }
+
+    loadBackgroundWidth(width)
+    {
+        for (let i=1;i<width;i++)
+        {
+            let temp1=new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png',i*719);
+            let temp2=new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png',i*719);
+            let temp3=new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png',i*719);
+            let temp4=new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png',i*719);
+            i++;
+           
+            this.backgroundObjects.push(temp1,temp2,temp3,temp4);
+            temp1=new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png',i*719);
+            temp2=new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png',i*719);
+            temp3=new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png',i*719);
+            temp4=new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png',i*719);
+            
+           
+            this.backgroundObjects.push(temp1,temp2,temp3,temp4);
+            i--;
+            temp1=new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png',-i*719);
+            temp2=new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png',-i*719);
+            temp3=new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png',-i*719);
+            temp4=new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png',-i*719);
+            i++;
+           
+            this.backgroundObjects.push(temp1,temp2,temp3,temp4);
+            temp1=new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png',-i*719);
+            temp2=new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png',-i*719);
+            temp3=new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png',-i*719);
+            temp4=new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png',-i*719);
+           
+            this.backgroundObjects.push(temp1,temp2,temp3,temp4);
+
         }
     }
 }
